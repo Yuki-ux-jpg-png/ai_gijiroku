3e7d3ab13609c20ae446234833a48c62d53fb5a9
Yuki-ux-jpg-png <58500553+Yuki-ux-jpg-png@users.noreply.github.com>
2026-06-16 12:56:08 +0900

    Update site.ts

--- lib/site.ts
+++ lib/site.ts
@@ -1,7 +1,9 @@
 export const site = {
   name: 'こころの居場所',
-  owner: '個人事業主',
+  owner: '渡辺 悠基',
   tradeName: 'こころの居場所',
+  address: '東京都江東区塩浜2丁目25-14 アルファプライム東陽町',
+  phone: '080-8731-6858',
   email: '',
   price: '月額50円',
   appUrl: process.env.NEXT_PUBLIC_APP_URL || ''
